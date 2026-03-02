6b0ae33fb35f3066fd869c82eec8dac6df27a6ac
Pavel <imhartash@gmail.com>
2026-03-02 10:46:15 +0700

    Defined InputSystem

--- FountEngine/src/systems/input/CInputSystem.cpp
+++ FountEngine/src/systems/input/CInputSystem.cpp
@@ -5,6 +5,58 @@ CInputSystem& CInputSystem::GetInstance() {
 	return Instance;
 }
 
-void CInputSystem::ProcessKeyMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	// TODO: Realisze Input System
+void CInputSystem::OnKeyDown(UINT uKey) {
+
+}
+
+void CInputSystem::OnKeyUp(UINT uKey) {
+
+}
+
+void CInputSystem::OnMouseMove(LPARAM lParam) {
+	
+}
+
+void CInputSystem::OnMouseButtonDown(UINT uButton) {
+
+}
+
+void CInputSystem::OnMouseButtonUp(UINT uButton) {
+
+}
+
+bool CInputSystem::IsKeyDown(UINT uKey) const {
+	return false;
+}
+
+bool CInputSystem::IsKeyPressed(UINT uKey) const {
+	return false;
+}
+
+bool CInputSystem::IsKeyReleased(UINT uKey) const {
+	return false;
+}
+
+int CInputSystem::GetMouseX() const {
+	return 0;
+}
+
+int CInputSystem::GetMouseY() const {
+	return 0;
+}
+
+bool CInputSystem::IsMouseButtonDown(UINT button) const {
+	return false;
+}
+
+void CInputSystem::Update() {
+
+}
+
+CInputSystem::CInputSystem() : m_nMouseX(0), m_nMouseY(0) {
+	m_vecKeys.fill(false);
+	m_vecPrevKeys.fill(false);
+
+	m_vecMouseButtons.fill(false);
+	m_vecPrevMouseButtons.fill(false);
 }
--- FountEngine/src/systems/input/CInputSystem.hpp
+++ FountEngine/src/systems/input/CInputSystem.hpp
@@ -1,14 +1,40 @@
 #pragma once
 #include <Windows.h>
+#include <array>
+#define KEY_COUNT 256
+#define MOUSE_BUTTON_COUNT 5
 
 class CInputSystem {
 public:
 	static CInputSystem& GetInstance();
 
-	void ProcessKeyMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
-	bool IsKeyDown(int key) const;
+	void OnKeyDown(UINT uKey);
+	void OnKeyUp(UINT uKey);
+
+	void OnMouseMove(LPARAM lParam);
+	void OnMouseButtonDown(UINT uButton);
+	void OnMouseButtonUp(UINT uButton);
+
+	bool IsKeyDown(UINT uKey) const;
+	bool IsKeyPressed(UINT uKey) const;
+	bool IsKeyReleased(UINT uKey) const;
+
+	int GetMouseX() const;
+	int GetMouseY() const;
+	bool IsMouseButtonDown(UINT button) const;
+
+	void Update();
 
 private:
+	std::array<bool, KEY_COUNT> m_vecKeys;
+	std::array<bool, KEY_COUNT> m_vecPrevKeys;
+
+	std::array<bool, MOUSE_BUTTON_COUNT> m_vecMouseButtons;
+	std::array<bool, MOUSE_BUTTON_COUNT> m_vecPrevMouseButtons;
+
+	int m_nMouseX;
+	int m_nMouseY;
+
 	CInputSystem();
-	~CInputSystem();
+	~CInputSystem() = default;
 };
